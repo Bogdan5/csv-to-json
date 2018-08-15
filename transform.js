9da562d76060522979862109750e02677b657a94
Bogdan5 <bogdan5x@gmail.com>
2018-08-14 22:42:53 -0400

    Comment

--- transform.js
+++ transform.js
@@ -3,17 +3,23 @@ const http = require('http');
 const url = 'https://prod-edxapp.edx-cdn.org/assets/';
 url += 'courseware/v1/07d100219da1a726dad5eddb090fa215/';
 url += 'asset-v1:Microsoft+DEV283x+2T2018+type@asset+block/customer-data.csv';
-http.get(url, (response) => {
-  let rawData = '';
-  response.on('data', (chunk) => {
-    rawData += chunk;
+let rawData = '';
+const fetchCSV = (urlA, callback) => {
+  http.get(urlA, (response) => {
+    response.on('data', (chunk) => {
+      rawData += chunk;
+    });
+    response.on('end', (response) => {
+      console.log('End stream');
+      callback(null, rawData);
+    });
+  }).on('error', (error) => {
+    console.log(`Error: ${error.message}`);
+    callback(error);
   });
-  response.on('end', (response) => {
-    console.log('End stream');
-  });
-}).on('error', (error) => {
-  console.log(`Error: ${error}`);
-});
+}
+
+
 
 var csvData = [];
 // fs.createReadStream(req.file.path)
