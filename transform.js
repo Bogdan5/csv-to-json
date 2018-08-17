a2a2db3f2f547b2c8ba48819dcdb6b7cf4de2f80
Bogdan5 <bogdan5x@gmail.com>
2018-08-16 22:01:21 -0400

    Comment

--- transform.js
+++ transform.js
@@ -1,20 +1,20 @@
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
-const csv = require('csv-parser');
+const csv = require('csvtojson');//transforms csv into JSON
 
 let url = 'https://prod-edxapp.edx-cdn.org/assets/';
 url += 'courseware/v1/07d100219da1a726dad5eddb090fa215/';
 url += 'asset-v1:Microsoft+DEV283x+2T2018+type@asset+block/customer-data.csv';
 let rawData = '';
+
+//a function that downloads the csv data into a newly-created file called file.html
 const fetchCSV = (urlA, callback) => {
   https.get(urlA, (response) => {
     response.on('data', (chunk) => {
-      // console.log(chunk);
       rawData += chunk;
     });
     response.on('end', (response) => {
-      console.log('End stream');
       callback(null, rawData);
     });
   }).on('error', (error) => {
@@ -22,6 +22,7 @@ const fetchCSV = (urlA, callback) => {
   });
 };
 
+//function called
 fetchCSV(url, (error, rawData) => {
   if (error) {
     console.log(`Error: ${error.message}`);
@@ -30,13 +31,10 @@ fetchCSV(url, (error, rawData) => {
   fs.writeFileSync(path.join(__dirname, 'file.html'), rawData);
 });
 
-var csvData = [];
-fs.createReadStream(path.join(__dirname, 'file.html'))
-  .pipe(csv())
-  .on('data', function (data) {
-    csvData.push(data);
-  })
-  .on('end', function () {
-    console.log('Writing file');
-    fs.writeFileSync(path.join(__dirname, 'csv.txt'), csvData);
-  });
+const csvFilePath = path.join(__dirname, 'file.html');
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj) => {
+  // it was necessary to stringify in order to avoid the [object, Object] format
+  fs.writeFileSync(path.join(__dirname, 'csv.html'), JSON.stringify(jsonObj));
+});
